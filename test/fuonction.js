f274409409206c62909f88e803f8d4bde30d0144
gavrielJaffe <55511597+gavrielJaffe@users.noreply.github.com>
2020-11-22 18:46:28 +0200

    Add files via upload

---
+++ test/fuonction.js
@@ -0,0 +1,18 @@
+//aleart function 
+function a()  {  
+    const name = "work ! " 
+     return alert (name);
+} 
+// creat an Arry of fruits 
+function b() {
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var names =["[]", "[]","[]"]
+var x = names.toString();
+document.getElementById("ArrayBtn").innerHTML = x;
+}
+// function of randon numbers -need to be use with the
+function getRndInteger(min, max) {
+return Math.floor(Math.random() * (max - min)) + min;
+
+}
+    
